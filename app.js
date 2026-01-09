336d418c0aff3b1d54fc903f6d7a6ae1f620ac73
Wissal <wissal1zabour@gmail.com>
2026-01-09 21:47:59 +0100

    CRUD users

--- app.js
+++ app.js
@@ -1,12 +1,87 @@
 import express from "express";
+import {body, validationResult} from "express-validator"
 
+
+let users= [
+    {
+        id : 1,
+        name : "wissal",
+        age :22,
+        adulte : true
+    },
+     {
+        id : 2,
+        name : "salsabil",
+        age :19,
+        adulte : true
+    },
+    {
+        id : 3,
+        name : "rayane",
+        age :17,
+        adulte : false
+    },
+]
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
 
+//get all useres
 
+app.get('/api/users',(req,res)=>{
+  res.json(users);
+})
 
+//get one user
 
+app.get('/api/users/:id',(req, res)=>{
+  let id = parseInt(req.params.id);
+  let user = users.find(user => user.id === id);
+
+  if(!user){
+    return res.status(404).json({error: "user doesnt exists"})
+  }
+  res.json(user);
+})
+
+//create a user
+app.post('/api/users',body('name').notEmpty() ,(req, res)=>{
+  let errors = validationResult(req);
+    if(!errors.isEmpty()){
+       return res.status(400).json({ errors: errors.array() });
+
+    }
+
+  let user = 
+  {
+    id : users.length + 1,
+    ...req.body
+  }
+   users.push(user);
+   res.status(201).json(users);
+
+})
+
+//update a user
+app.put('/api/users/:id',(req,res)=>{
+  let id = parseInt(req.params.id);
+  let user = users.find(user => user.id === id);
+ 
+  user = {
+    ...user, 
+    ...req.body
+  }
+ res.json(user);
+})
+
+//delete user
+
+app.delete('/api/users/:id',(req, res)=>{
+  let id = parseInt(req.params.id);
+  users = users.filter((user)=> user.id !== id);
+  res.json(users);
+})
 
 app.listen(PORT,'localhost',()=>{
     console.log(`the server is running at port ${PORT}`);
